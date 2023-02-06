3fa34a74726bde4139fdf0af2c5830f8406e4a9f
KevayneCst <59823422+KevayneCst@users.noreply.github.com>
2023-02-06 23:30:14 +0100

    Added V1

---
+++ V1/khepera2/controllers/v1_controller/build/release/v1_controller.d
@@ -0,0 +1,7 @@
+build/release/v1_controller.o: v1_controller.c \
+  /Applications/Webots.app/Contents/include/controller/c/webots/distance_sensor.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/types.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/motor.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/robot.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/nodes.h \
+  /Applications/Webots.app/Contents/include/controller/c/webots/light_sensor.h
